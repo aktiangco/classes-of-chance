a90e4cace67b78572c07855035715321754a677a
aktiangco <113158472+aktiangco@users.noreply.github.com>
2022-11-21 19:56:41 -0800

    activity

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
 class Casino {
     // Write code here
-};
+    constructor(name, timesPlayed) {
+        this.name = name;
+        this.timesPlayed = 0;
+        //winnings = 0;
+    }
+    playGame(betAmount) {
+        // increment times played
+        this.timesPlayed += 1;
+        //check if something is less <= .5
+        // if less than log casino wins
+        if (Math.random() <= 0.5) {
+            console.log(this.name + ' wins :(')
+        } else {
+            // change to betAmount * (this.timePlayed + 1)
+            let payout = betAmount * this.timesPlayed
+            console.log(`you win $${payout}!!!!`)
+        }
+    }
+}
+
+let codeCasino = new Casino("Code Casino")
+
+codeCasino.playGame(20);
+codeCasino.playGame(20);
+codeCasino.playGame(20);
+codeCasino.playGame(20);
+
+
 
 // TESTS
-const myCasino = new Casino("HackerU Casino");
-console.log(myCasino);
-myCasino.playGame(5);
-myCasino.playGame(15);
-myCasino.playGame(25);
-myCasino.playGame(35);
+// const myCasino = new Casino("HackerU Casino");
+// console.log(myCasino);
+// myCasino.playGame(5);
+// myCasino.playGame(15);
+// myCasino.playGame(25);
+// myCasino.playGame(35);
 
 // BONUS TESTS
 /*
